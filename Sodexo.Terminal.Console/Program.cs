de5b74096067e07cc3b035714bd023b21cd92e8e
Jojo <jonas.uliana@passwd.com.br>
2020-10-03 22:35:46 -0300

    Added Sodexo.Terminal.Console as project CLI interface

---
+++ Sodexo.Terminal.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sodexo.Terminal.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
